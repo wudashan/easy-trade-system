75257612a4a2e31c34171beb667ff091e2106512
wudashan <wudashan0825@foxmail.com>
2017-11-19 09:57:49 +0800

    增加请求参数

--- src/main/java/cn/wudashan/controller/TradeController.java
+++ src/main/java/cn/wudashan/controller/TradeController.java
@@ -1,6 +1,8 @@
 package cn.wudashan.controller;
 
 import cn.wudashan.dto.TradeRequestDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 
 @RestController
 public class TradeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping(path = "/v1/trade", method= RequestMethod.POST)
-    public ResponseEntity<Void> trade(@RequestBody TradeRequestDTO requestDTO) {
+    public ResponseEntity<Void> trade(@RequestBody @Valid TradeRequestDTO requestDTO) {
+        logger.info("request:{}", requestDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- src/main/java/cn/wudashan/dto/TradeRequestDTO.java
+++ src/main/java/cn/wudashan/dto/TradeRequestDTO.java
@@ -1,24 +1,40 @@
 package cn.wudashan.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * @author wuzhaofeng
+ */
 public class TradeRequestDTO {
 
-    private Long userId;
+    @NotNull
+    @Size(min = 1, max = 64)
+    private String userId;
 
+    @NotNull
     private Double foreignAmount;
 
+    @NotNull
+    @Pattern(regexp = "USD|HKD")
     private String foreignAmountType;
 
+    @NotNull
     private Double rmbAmount;
 
+    @NotNull
     private Double exchangeRate;
 
-    private String tradeDirection;
+    @NotNull
+    @Pattern(regexp = "buy|sell")
+    private String foreignTradeDirection;
 
-    public Long getUserId() {
+    public String getUserId() {
         return userId;
     }
 
-    public void setUserId(Long userId) {
+    public void setUserId(String userId) {
         this.userId = userId;
     }
 
@@ -54,12 +70,24 @@ public class TradeRequestDTO {
         this.exchangeRate = exchangeRate;
     }
 
-    public String getTradeDirection() {
-        return tradeDirection;
+    public String getForeignTradeDirection() {
+        return foreignTradeDirection;
     }
 
-    public void setTradeDirection(String tradeDirection) {
-        this.tradeDirection = tradeDirection;
+    public void setForeignTradeDirection(String foreignTradeDirection) {
+        this.foreignTradeDirection = foreignTradeDirection;
     }
 
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("TradeRequestDTO{");
+        sb.append("userId=").append(userId);
+        sb.append(", foreignAmount=").append(foreignAmount);
+        sb.append(", foreignAmountType='").append(foreignAmountType).append('\'');
+        sb.append(", rmbAmount=").append(rmbAmount);
+        sb.append(", exchangeRate=").append(exchangeRate);
+        sb.append(", foreignTradeDirection='").append(foreignTradeDirection).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
